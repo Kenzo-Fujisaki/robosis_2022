2e14aed68dccab397d7d2c170da76763ee45ad0a
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2022-10-27 12:39:39 +0900

    Complete test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 # SPDX-FileCopyrightText: 2022 Kenzo Fujisaki
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -6,12 +6,20 @@ ng () {
         echo NG at Line $1
         res=1
 }
-　
 res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
