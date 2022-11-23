79bff7bfb6cef835d3f782f4c5e5a4b142e80a29
Kenzo Fujisaki <s21c1103bk@s.chibakoudai.jp>
2022-11-23 11:28:32 +0900

    OK test

--- test_calc.bash
+++ test_calc.bash
@@ -9,30 +9,30 @@ ng () {
 res=0
 
 ### Method of calculation　###
-out=$(echo -e '1\n+\n3' | ./calc) #和
+out=$(echo -e '1\n+\n3\n=' | ./calc) #和
 [ "${out}" = 4.0 ] || ng ${LINENO}
 
-out=$(echo -e '1\n-\n3' | ./calc) #差
+out=$(echo -e '1\n-\n3\n=' | ./calc) #差
 [ "${out}" = -2.0 ] || ng ${LINENO}
 
-out=$(echo -e '1\n*\n3' | ./calc) #乗
+out=$(echo -e '1\n*\n3\n=' | ./calc) #乗
 [ "${out}" = 3.0 ] || ng ${LINENO}
 
-out=$(echo -e '3\n/\n1' | ./calc) #除
+out=$(echo -e '3\n/\n1\n=' | ./calc) #除
 [ "${out}" = 3.0 ] || ng ${LINENO}
 
-out=$(echo -e '3\n@\n1' | ./calc) #不適切な演算子ではエラーを出す
+out=$(echo -e '3\n@\n1\n=' | ./calc) #不適切な演算子ではエラーを出す
 [ "${out}" = error ] || ng ${LINENO}
 
-out=$(echo -e '3\n/\n0' | ./calc) #0で割られた際にもエラーを出す
+out=$(echo -e '3\n/\n0\n=' | ./calc) #0で割られた際にもエラーを出す
 [ "${out}" = error ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
-out=$(echo -e 'あ\n+\n1' | ./calc) #１つ目の入力が数字以外
+out=$(echo -e 'あ\n+\n1\n=' | ./calc) #１つ目の入力が数字以外
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo -e '3\n+\nあ' | ./calc) #２つ目の入力が数字以外
+out=$(echo -e '3\n+\nあ\n=' | ./calc) #２つ目の入力が数字以外
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
